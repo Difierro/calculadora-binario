b823b4ab1378c2c7438f0ee4c7074aaa8e4f1930
difierro <gustavo7867grdr@gmail.com>
2023-09-28 23:01:37 -0300

    correções de bugs e de lógica

--- calcbin.c
+++ calcbin.c
@@ -1,58 +1,41 @@
-// aparece local da memória
-
 #include <stdio.h>
-int arrayLength();
-char calc();
+int arrayLength(); //função para definir o tamanho do array de acordo com o numero decimal
 
 int main(){
-    int nd;
-    int ibl, iblII;
+    int numeroDecimal;
+    int ibl = 0; //indice de alocação dos 0's e 1's
+    int tamanhoArray; //tamanho do array
     
     printf("Calculadora Decimal para Binario: \n");
     printf("Digite o numero inteiro decimal: ");
-    scanf("%d", &nd);
+    scanf("%d", &numeroDecimal);
     
-    iblII = arrayLength(nd);
-    ibl = iblII;
-    char binario[ibl];
+    tamanhoArray = arrayLength(numeroDecimal);
+    char binario[tamanhoArray];
     
-    while(nd >= 1){
-        binario[ibl] = calc(nd);
-        nd = nd/2;
+    while(numeroDecimal >= 1){
+        if (numeroDecimal % 2 == 0){
+            binario[ibl] = '0';
+        }else{
+            binario[ibl] = '1';
+        }
+        ibl++;
+        numeroDecimal = numeroDecimal / 2;
     }
 
-    for(int i =0; i< iblII; i++){
-        printf("%d", binario[i]);
+    for(int i = tamanhoArray-1; i >= 0; i--){
+        printf("%c ", binario[i]);
     }
-    
-
-    
 
     return 0; 
 }
 
-int arrayLength(int nd){
-    int ib;
-    while(nd >= 1){
-        if (nd % 2 == 0){
-            nd= nd/2;
-        }else{
-            nd= nd/2;
-        }
+int arrayLength(int numeroDecimal){
+    int ib = 0;
+    while(numeroDecimal >= 1){ 
+        numeroDecimal= numeroDecimal / 2;
         ib++;
     }
 
     return ib;
 }
-
-char calc(int nd){
-    char numBinario;
-        if (nd % 2 == 0){
-            numBinario = '0';
-        }else{
-            numBinario = '1';
-        }
-        return numBinario;
-}
-
-//8 = 1000
